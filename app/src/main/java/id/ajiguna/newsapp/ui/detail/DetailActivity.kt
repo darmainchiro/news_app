ba69b72eb5f9fae1a9d0348ce3cd786c44fc5b45
Aji Guna <gunaitsugo@gmail.com>
2023-08-07 22:52:19 +0700

    add detail

--- app/src/main/java/id/ajiguna/newsapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/ajiguna/newsapp/ui/detail/DetailActivity.kt
@@ -2,11 +2,70 @@ package id.ajiguna.newsapp.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import id.ajiguna.newsapp.R
+import id.ajiguna.newsapp.databinding.ActivityDetailBinding
+import id.ajiguna.newsapp.databinding.CustomToolbarBinding
+import id.ajiguna.newsapp.source.news.ArticleModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.dsl.module
 
+val detailModule = module {
+    factory { DetailActivity() }
+}
 class DetailActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityDetailBinding.inflate(layoutInflater) }
+    private lateinit var bindingToolbar: CustomToolbarBinding
+    private val viewModel: DetailViewModel by viewModel()
+    private val detail by lazy {
+        intent.getSerializableExtra("detail") as ArticleModel
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        bindingToolbar = binding.toolbar
+        setContentView(binding.root)
+
+        setSupportActionBar(bindingToolbar.container)
+        supportActionBar!!.apply {
+            title = ""
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        detail.let {
+            viewModel.find(it)
+            val web = binding.webView
+            web.loadUrl(it.url!!)
+            web.webViewClient = object : WebViewClient(){
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.progressTop.visibility = View.GONE
+                }
+            }
+            val settings = binding.webView.settings
+            settings.javaScriptCanOpenWindowsAutomatically = false
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bookmark, menu)
+        val menuBookmark = menu!!.findItem(R.id.action_bookmark)
+        menuBookmark.setOnMenuItemClickListener {
+            viewModel.bookmark(detail)
+            true
+        }
+        viewModel.isBookmark.observe(this) {
+            if (it == 0) menuBookmark.setIcon(R.drawable.ic_bookmark_add)
+            else menuBookmark.setIcon(R.drawable.ic_bookmark_added)
+        }
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/id/ajiguna/newsapp/ui/detail/DetailViewModel.kt
@@ -0,0 +1,33 @@
+package id.ajiguna.newsapp.ui.detail
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import id.ajiguna.newsapp.source.news.ArticleModel
+import id.ajiguna.newsapp.source.news.NewsRepository
+import kotlinx.coroutines.launch
+import org.koin.dsl.module
+
+val detailViewModule = module {
+    factory { DetailViewModel(get()) }
+}
+class DetailViewModel(
+    private val repository: NewsRepository
+): ViewModel() {
+    val isBookmark by lazy { MutableLiveData(0) }
+
+    fun find(articleModel: ArticleModel) {
+//        viewModelScope.launch {
+//            isBookmark.value = repository.find(articleModel)
+//        }
+    }
+
+    fun bookmark(articleModel: ArticleModel){
+        viewModelScope.launch {
+//            if (isBookmark.value == 0) repository.save((articleModel))
+//            else repository.remove(articleModel)
+//            find(articleModel)
+
+        }
+    }
+}
